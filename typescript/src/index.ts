709cf7d1872221066206cdf2a1889fe27d80ab07
ula <haowenz@andrew.cmu.edu>
2024-09-06 12:10:12 -0400

    Complete editting the typescript main program to print areas of rectangle and circle

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,15 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { Rectangle} from "./shapes/rectangle.js";
+import { Circle } from "./shapes/circle.js";
+import { Square } from "./shapes/square.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
+const rectangle: Rectangle = new Rectangle(2, 3);
+const recRenderer = newRenderer(rectangle);
+recRenderer.draw();
+
+const circle: Circle = new Circle(2);
+const circleRenderer = newRenderer(circle);
+circleRenderer.draw();
+
+
+// index.ts

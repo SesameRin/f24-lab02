51d7973f60d9c30715ade543807ed1eb7753859c
ula <haowenz@andrew.cmu.edu>
2024-09-06 12:05:28 -0400

    Complete editting the main java program

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer recRenderer = new Renderer(rectangle);
+        Renderer circleRenderer = new Renderer(circle);
 
-        renderer.draw();
+        recRenderer.draw();
+        circleRenderer.draw();
     }
 }
